4efe1304a4a356382081538cf975bad6ccdfa462
Jonathan Lutz <>
2021-02-22 20:32:25 -0500

    Add Gallery Photos

--- src/pages/Industrial.js
+++ src/pages/Industrial.js
@@ -1,55 +1,97 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
+import pic1 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-001.jpg";
+import pic2 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-002.jpg";
+import pic3 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-003.jpg";
+import pic4 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-004.jpg";
+import pic5 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-005.jpg";
+import pic6 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-006.jpg";
+import pic7 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-007.jpg";
+import pic8 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-008.jpg";
+import pic9 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-009.jpg";
+import pic10 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-010.jpg";
+import pic11 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-011.jpg";
+import pic12 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-012.jpg";
+import pic13 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-013.jpg";
+import pic14 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-014.jpg";
+import pic15 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-015.jpg";
+import pic16 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-016.jpg";
+import pic17 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-017.jpg";
+import pic18 from "../assets/gallery-photos/location-cool-places/Location-Cool-Places-018.jpg";
 
 const Industrial = () => {
   const images = [
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic1,
+      thumbnail: pic1,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic2,
+      thumbnail: pic2,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic3,
+      thumbnail: pic3,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic4,
+      thumbnail: pic4,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic5,
+      thumbnail: pic5,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic6,
+      thumbnail: pic6,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic7,
+      thumbnail: pic7,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic8,
+      thumbnail: pic8,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic9,
+      thumbnail: pic9,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic10,
+      thumbnail: pic10,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic11,
+      thumbnail: pic11,
     },
     {
-      original: "https://loremflickr.com/1000/600/business",
-      thumbnail: "https://loremflickr.com/250/150/business",
+      original: pic12,
+      thumbnail: pic12,
+    },
+    {
+      original: pic13,
+      thumbnail: pic13,
+    },
+    {
+      original: pic14,
+      thumbnail: pic14,
+    },
+    {
+      original: pic15,
+      thumbnail: pic15,
+    },
+    {
+      original: pic16,
+      thumbnail: pic16,
+    },
+    {
+      original: pic17,
+      thumbnail: pic17,
+    },
+    {
+      original: pic18,
+      thumbnail: pic18,
     },
   ];
   return (
